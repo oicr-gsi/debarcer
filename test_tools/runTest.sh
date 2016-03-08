542506ce492911a2a41eec79ea9b1c94dd28c263
Paul Krzyzanowski <paul.krzyzanowski@oicr.on.ca>
2016-03-08 17:16:56 -0500

    Added a fastq file containing sequence data for test runs

--- test_tools/runTest.sh
+++ test_tools/runTest.sh
@@ -2,8 +2,7 @@
 
 mkdir -p testresults
 cd testresults
-cp /u/pkrzyzanowski/projects/simsenseq/debarcer/demodata/Sample_9297.R1.fastq.gz .
 
-module load debarcer/0.2.0
-runDebarcer.sh Sample_9297.R1.fastq.gz Sample_9297
-
+module load debarcer/dev
+cp $BHOME/test_tools/Sample_Test.R1.fastq.gz .
+runDebarcer.sh Sample_Test.R1.fastq.gz Sample_Test
