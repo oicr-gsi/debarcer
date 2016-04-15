7d19efd8a12e45ab8fae148a724df570df77b8b0
Paul Krzyzanowski <paul.krzyzanowski@oicr.on.ca>
2016-04-15 13:08:59 -0400

    Updated tools/populateWorkDirectory.pl to use new runDebarcer.sh launch syntax

--- tools/populateWorkDirectory.pl
+++ tools/populateWorkDirectory.pl
@@ -18,6 +18,7 @@ Email: paul.krzyzanowski@oicr.on.ca
 
 use Getopt::Long;
 use File::Basename;
+use Cwd;
 
 my $fastq_source = shift @ARGV or die "\nNeed to provide a directory with fastq.gz files to process...\n\n";
 my @fastqs = `ls $fastq_source/*_R1_001.fastq.gz`;
@@ -36,23 +37,24 @@ print RELAUNCHSCRIPT "LAUNCHDIR=`pwd`\n\n";
 foreach my $fastq ( @fastqs ) {
 
 	chomp $fastq;
+	my $thisdir = cwd();
+	my $fastq_abs_name = $thisdir . "/" . $fastq;
 
 	my $samplename = basename($fastq, "_R1_001.fastq.gz");
 	my $basefile = basename($fastq);
 	print STDERR "$samplename $basefile\n";
 
 	system("mkdir -p results/$samplename");
-	system("ln -s ../../$fastq results/$samplename/$basefile");
-
+	
 	open LAUNCHSCRIPT, ">results/$samplename/launchDebarcer.sh";
-	print LAUNCHSCRIPT "qsub -N \"Dbarc_$samplename\" -l h_vmem=32G -cwd -b y \"module load debarcer/$module_version; runDebarcer.sh $basefile $samplename\"";
+	print LAUNCHSCRIPT "qsub -N \"Dbarc_$samplename\" -l h_vmem=32G -cwd -b y \"module load debarcer/$module_version; runDebarcer.sh -r -f $fastq_abs_name -n $samplename -o .\"";
 	close LAUNCHSCRIPT;
 	
 	open CONFIGSCRIPT, ">results/$samplename/debarcer.conf";
 
 	print CONFIGSCRIPT <<BLOCK
 # Debarcer override file.  The master process stores these variables in the config array.
-plexity=5
+plexity=50
 BLOCK
 	;
 	close CONFIGSCRIPT;
