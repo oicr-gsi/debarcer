69b90acd14f0314dbefd066d7efb85016cfa16ee
Paul Krzyzanowski <paul.krzyzanowski@oicr.on.ca>
2017-04-21 12:07:24 -0400

    Barcodes are now added to a new R1 file in a user specified output directory

--- src/combineHaloplexFastqs.pl
+++ src/combineHaloplexFastqs.pl
@@ -11,10 +11,13 @@ Combine HaloplexHS barcode fastqs with read fastqs
 
 use Getopt::Long;
 use Data::Dumper;
+use File::Basename;
 use FindBin;
 use lib "$FindBin::Bin/";
 use Debarcer;
 use Config::General qw(ParseConfig);
+
+print STDERR "Starting combineHaloplexFastqs.pl\n";
 
 my %args = ();
 GetOptions(
@@ -23,8 +26,9 @@ GetOptions(
 	"R2file=s" => \$args{"R2file"},   
 	"barcodefastq=s" => \$args{"barcodeFq"}   
 );
-
 my %haloplexBarcodes = ();
+my $outputfile = '';
+
 open BARCODES, "gunzip -c ".$args{"barcodeFq"}." |";
 while (<BARCODES>) {
 	if (/^@/) {  # If we've hit a read entry the next line is the HaloplexHS barcode
@@ -39,3 +43,23 @@ close BARCODES;
 print STDERR keys(%haloplexBarcodes) . " HaloplexHS barcodes found\n";
 
 
+# Add the barcodes to the Read 1 file
+open INFILE, "gunzip -c ".$args{"R1file"}." |";
+$outputfile = $args{"outputdir"}."/". basename($args{"R1file"});
+$outputfile =~ s/_R\d_/_R1_/;  # Ensure the file indicated R1
+$outputfile =~ s/\.gz$//; # output uncompressed fastq
+open OUTFILE, ">$outputfile";
+while (<INFILE>) {
+	if (/^@/) {  # If we've hit a read entry the next line is the HaloplexHS barcode
+		my @readHeader = split(/\s/, $_);
+		$readHeader[0] .= ":HaloplexHS-".$haloplexBarcodes{$readHeader[0]};
+		print OUTFILE join(" ", @readHeader) . "\n";
+	} else {
+		print OUTFILE;
+	}
+}
+close INFILE;
+close OUTFILE;
+system("gzip $outputfile");
+print STDERR "Added HaloplexHS barcodes to R1 file\n";
+ 
