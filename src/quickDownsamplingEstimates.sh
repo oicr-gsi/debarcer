a7772d08ac073b56f7b1901b308f1f60b9907f60
Paul Krzyzanowski <paul.krzyzanowski@oicr.on.ca>
2017-04-21 11:03:26 -0400

    Load R module

--- src/quickDownsamplingEstimates.sh
+++ src/quickDownsamplingEstimates.sh
@@ -1,6 +1,7 @@
 
 
 # Generate the downsampling tables using the *UIDdepths.txt.gz file.
+module load R/3.3.0
 
 SAMPLENAME=$1
 Rscript $BHOME/src/quickDownsamplingEstimates.R ./tables/$SAMPLENAME.UIDdepths.txt.gz
