487e55eb4aae452ac0c66cbe433b7901ffc5938b
Theodore Bodak <Theodore.Bodak@oicr.on.ca>
2018-03-23 11:36:19 -0400

    Update debarcer.py, preprocess_fastqs.py

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # debarcer
 Debarcer: A package for De-Barcoding and Error Correction of sequencing data containing molecular barcodes
 
-Usage
+Typical Workflow
 -----
-`./debarcer/src/debarcer.sh -r chrN:posA-posB -c /path/to/config.ini -b /path/to/bam_file.bam -o /path/to/output_dir`
+```shell
+## Preprocess some fastq files
+python debarcer.py P -o /path/to/output_dir -r1 /path/to/read1.fastq -r2 /path/to/read2.fastq -prepname "prepname" -prepfile /path/to/library_prep_types.ini
 
-A default config file is provided in `/debarcer/config`.
+## Align, sort, index 
+## ...
+## produces: bam_file.bam, bam_file.bam.bai
+
+## Run Debarcer 
+python debarcer.py D -t -r chrN:posA-posB -c /path/to/config.ini  -be /path/to/bed_file.bed -b /path/to/bam_file.bam -o /path/to/output_dir
+```
+
+A sample config file is provided in `/debarcer/config/sample_config.ini`, and a sample prepfile is provided in `/debarcer/config/library_prep_types.ini`.
--- debarcer.py
+++ debarcer.py
@@ -9,15 +9,6 @@ Debarcer (De-Barcoding and Error Correction) is a package
 for working with next-gen sequencing data that contains
 molecular barcodes.
 
-Usage
------
---tally        Run a UMI tally (UMI_count.py). Necessary for later steps.
---bam_file     Path to your BAM file.
---bed_file     Path to your BED file.
---region       Region to analyze (string of the form chrX:posA-posB).
---output_path  Path to write output files to.
---config       Path to your config file.
-
 Author: Theodore Bodak
 Copyright (c) 2018 GSI, Ontario Institute for Cancer Research
 """
@@ -27,19 +18,11 @@ import configparser
 from src.handle_args import handle_arg
 from src.UMI_count import generate_tally_output
 from src.generate_consensus import generate_consensus_output
+from src.preprocess_fastqs import reheader_fastqs
 
-if __name__ == '__main__':
 
-	## Argument + config parsing and error handling
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-t',  '--tally',       help='Run a UMI tally (UMI_count.py).', action='store_true')
-	parser.add_argument('-b',  '--bam_file',    help='Path to your BAM file.', required=True)
-	parser.add_argument('-be', '--bed_file',    help='Path to your BED file.', required=True)
-	parser.add_argument('-r',  '--region',      help='Region to analyze (string of the form chrX:posA-posB).', required=True)
-	parser.add_argument('-o',  '--output_path', help='Path to write output files to.', required=True)
-	parser.add_argument('-c',  '--config',      help='Path to your config file.')
-
-	args = parser.parse_args()
+def debarcer(args):
+	"""Main Debarcer analysis mode."""
 
 	if args.config:
    		config = configparser.ConfigParser()
@@ -59,7 +42,7 @@ if __name__ == '__main__':
 	bam_file    = handle_arg(args.bam_file, config['PATHS']['bam_file'] if config else None, 
 					'No BAM file provided in args or config.')
 	bed_file    = handle_arg(args.bed_file, config['PATHS']['bed_file'] if config else None, 
-					'No BED file providid in args or config.')
+					'No BED file provided in args or config.')
 	output_path = handle_arg(args.output_path, config['PATHS']['output_path'] if config else None, 
 					'No output path provided in args or config.')
 
@@ -87,3 +70,47 @@ if __name__ == '__main__':
 
 	## TODO plots and additional output/stats
 	## ...
+
+def preprocess(args):
+	"""Preprocess mode for processing fastq files."""
+
+	reheader_fastqs(
+		r1_file=args.r1_file, 
+		r2_file=args.r2_file, 
+		r3_file=args.r3_file, 
+		output_path=args.output_path, 
+		prepname=args.prepname, 
+		prepfile=args.prepfile)
+
+
+if __name__ == '__main__':
+
+	## Argument + config parsing and error handling
+	parser = argparse.ArgumentParser(description="A package for De-Barcoding and Error Correction" \
+	 											 " of sequencing data containing molecular barcodes.")
+	subparsers = parser.add_subparsers()
+
+	## Main Debarcer command - requires BAM file 
+	d_parser = subparsers.add_parser('D', help="Main Debarcer analysis mode.")
+	d_parser.add_argument('-t', '--tally', help='Run a UMI tally (UMI_count.py).', action='store_true')
+	d_parser.add_argument('-o', '--output_path', help='Path to write output files to.', required=True)
+	d_parser.add_argument('-r', '--region', help='Region to analyze (string of the form chrX:posA-posB).', required=True)
+	d_parser.add_argument('-be', '--bed_file', help='Path to your BED file.', required=True)
+	d_parser.add_argument('-b', '--bam_file', help='Path to your BAM file.', required=True)
+	d_parser.add_argument('-c', '--config', help='Path to your config file.')
+	d_parser.set_defaults(func=debarcer)
+
+	## Preprocess command - requires unprocessed fastq files
+	p_parser = subparsers.add_parser('P', help="Preprocess mode for processing fastq files.")
+	p_parser.add_argument('-o', '--output_path', help='Path to write updated fastq files to.', required=True)
+	p_parser.add_argument('-r1', '--read1', help='Path to first FASTQ file.', required=True)
+	p_parser.add_argument('-r2', '--read2', help='Path to second FASTQ file, if applicable.')
+	p_parser.add_argument('-r3', '--read3', help='Path to third FASTQ file, if applicable.')
+	p_parser.add_argument('-p', '--prepname', help='Name of library prep to  use (defined in library_prep_types.ini).', required=True)
+	p_parser.add_argument('-pf', '--prepfile', help='Path to your library_prep_types.ini file.', required=True)
+	p_parser.set_defaults(func=preprocess)
+
+	args = parser.parse_args()
+	args.func(args)
+
+	
