cd3f297ee34a94663ef7a5a61580a67db35ae337
iwarikoo <ishawarikoo@gmail.com>
2018-12-07 13:24:53 -0500

    Corrected reference sequence Debarcer output

--- debarcer/src/get_ref_seq.py
+++ debarcer/src/get_ref_seq.py
@@ -4,6 +4,6 @@ import pysam
 def get_ref_seq(contig, region_start, region_end, config):
     """Gets reference sequence on a region from reference file."""
     with pysam.FastaFile(config['PATHS']['reference_file']) as reader:
-        ref_seq = reader.fetch(contig, region_start, region_end).upper()
+        ref_seq = reader.fetch(contig, region_start-1, region_end).upper()
 
     return ref_seq
