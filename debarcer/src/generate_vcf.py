834756f4246985ccce11294af02c696e2dbd5d16
Richard Jovelin <richard.jovelin@oicr.on.ca>
2019-06-18 13:33:45 -0400

    removed unused modules

--- debarcer/src/generate_vcf.py
+++ debarcer/src/generate_vcf.py
@@ -1,4 +1,3 @@
-
 import sys
 import pysam
 import configparser
@@ -8,7 +7,7 @@ import functools
 import csv
 from src.generate_consensus import ConsDataRow
 from src.get_ref_seq import get_ref_seq
-from src.umi_error_correct import UMIGroup
+#from src.umi_error_correct import UMIGroup
 from src.get_consensus_seq import get_consensus_seq, get_uncollapsed_seq
 from src.handle_args import handle_arg
 from src.get_ref_seq import get_ref_seq
