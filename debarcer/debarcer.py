59603d3f4065010726d72c24dd62b29ef1ab6555
Richard Jovelin <richard.jovelin@oicr.on.ca>
2019-06-18 13:55:48 -0400

    check positions in region

--- debarcer/debarcer.py
+++ debarcer/debarcer.py
@@ -144,11 +144,11 @@ def group_umis(args):
     if contig[:len('chr')] != 'chr' and contig[len('chr'):] not in chromos:
         raise ValueError('ERR: Incorrect chromosome name (should look like chr1:1200000-1250000)')
     # get region coordinates. use 1-based inclusive. this will be converted to 0-based by pysam   
-    region_start = int(region.split(":")[1].split("-")[0])
-    region_end = int(region.split(":")[1].split("-")[1])
+    region_start, region_end = region.split(":")[1].split("-")[0], region.split(":")[1].split("-")[1]
     if region_start.isnumeric() == False or region_end.isnumeric() == False:
         raise ValueError('ERR: Incorrect start and end coordinates (should look like chr1:1200000-1250000)')
-    
+    region_start, region_end = int(region_start), int(region_end)
+        
     
     # get umi position and distance thresholds from config
     try:
