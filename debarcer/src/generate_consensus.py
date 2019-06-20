c6bfefad5981960767bcd8774b46ae5acc329578
Richard Jovelin <richard.jovelin@oicr.on.ca>
2019-06-20 12:00:01 -0400

    cleaned up modules import

--- debarcer/src/generate_consensus.py
+++ debarcer/src/generate_consensus.py
@@ -1,16 +1,9 @@
-
 import sys
 import pysam
 import configparser
 import argparse
 import operator
 import functools
-#from src.umi_error_correct import UMIGroup
-from src.get_consensus_seq import get_consensus_seq, get_uncollapsed_seq
-from src.handle_args import handle_arg
-from src.get_ref_seq import get_ref_seq
-
-
 
 
 def get_ref_seq(contig, region_start, region_end, config):
