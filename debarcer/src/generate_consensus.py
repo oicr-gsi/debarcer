d4147bec17fb9b0a8a01759b505daca34433727a
rjovelin <richard.jovelin@oicr.on.ca>
2019-06-26 12:59:27 -0400

    fixed issue #126

--- debarcer/src/generate_consensus.py
+++ debarcer/src/generate_consensus.py
@@ -132,6 +132,7 @@ def get_consensus_seq(umi_families, fam_size, ref_seq, contig, region_start, reg
                                     # add base info
                                     curr_pos = pos + 1
                                     allele = (ref_base, alt_base)
+                                    # count the number of reads supporting this allele
                                     if curr_pos not in consensus_seq:
                                         consensus_seq[curr_pos] = {}
                                     if family_key not in consensus_seq[curr_pos]:
@@ -192,6 +193,7 @@ def get_uncollapsed_seq(ref_seq, contig, region_start, region_end, bam_file, max
                     # add base info
                     curr_pos = pos + 1
                     allele = (ref_base, alt_base)
+                    # count the number of reads supporting this allele
                     if curr_pos not in uncollapsed_seq:
                         uncollapsed_seq[curr_pos] = {}
                     if allele not in uncollapsed_seq[curr_pos]:
@@ -257,6 +259,61 @@ class ConsDataRow:
         return self.stats
 
 
+
+
+
+
+def get_count_per_group(umi_families):
+    '''
+    (dict) -> dict
+    
+    :param umi_families: Information about each umi: parent umi and positions,
+                         counts of each family within a given group
+                         positions are 0-based half opened
+    
+    Return a dictionary umi count per position and group (ie. parent)
+    '''
+      
+    count_per_group = {}
+    for umi in umi_families:
+        parent = umi_families[umi]['parent']
+        if parent not in count_per_group:
+            count_per_group[parent] = {}
+        for pos in umi_families[umi]['positions']:
+            count_per_group[parent][int(pos)] = umi_families[umi]['positions'][pos]
+    return count_per_group
+
+
+def get_fam_size(count_per_group, position, fam_size):
+    '''
+    (dict, int, int) -> tuple
+    
+    :param count_per_group: umi family count per parent group and position
+    :position: A given position in a genomic region. 0-based half opened
+    :fam_size: Family size threshold to accept umi
+    
+    Return a tuple with minimum and mean umi family size for the given position
+    '''
+    
+    # umis belonging to a same group, ie having same parent, have the same count
+    # need to count umis at the parent level to avoid redundant counts
+    
+    # make a list of umi count for a given position
+    L = []
+    for parent in count_per_group:
+        for pos in count_per_group[parent]:
+            # check if given position
+            if pos == position:
+                # check if umi count greater than fam_size threshold
+                if count_per_group[parent][pos] >= fam_size:
+                    L.append(count_per_group[parent][pos])
+    # compute minimum and mean family size
+    min_fam_size, mean_fam_size = min(L), sum(L) / len(L)
+    
+    return (min_fam_size, mean_fam_size)
+    
+
+
 def generate_consensus(umi_families, fam_size, ref_seq, contig, region_start, region_end, bam_file, pos_threshold, percent_threshold, count_threshold, max_depth=1000000, truncate=True, ignore_orphans=True):
     """
     
@@ -275,9 +332,9 @@ def generate_consensus(umi_families, fam_size, ref_seq, contig, region_start, re
     ## Values: tables of A,T,C,G (etc) counts from each UMI+Pos family
     consensus_seq = get_consensus_seq(umi_families, fam_size, ref_seq, contig, region_start, region_end, bam_file, pos_threshold, max_depth=max_depth, truncate=truncate, ignore_orphans=ignore_orphans)
 
-    
 
-
+    # get the count of umi families per group and position
+    group_count = get_count_per_group(umi_families)
 
     cons_data = {}
 
@@ -289,8 +346,10 @@ def generate_consensus(umi_families, fam_size, ref_seq, contig, region_start, re
 
             consensuses = {}
             raw_depth = 0
-            min_fam = min([sum(consensus_seq[base_pos][fam].values()) for fam in consensus_seq[base_pos]]) 
             
+            # compute minimum and mean family size
+            min_fam, mean_fam = get_fam_size(group_count, base_pos, fam_size) 
+                        
             for family in consensus_seq[base_pos]:
                        
                 cons_allele = max(consensus_seq[base_pos][family].items(), key = operator.itemgetter(1))[0]
@@ -308,8 +367,6 @@ def generate_consensus(umi_families, fam_size, ref_seq, contig, region_start, re
                         consensuses[cons_allele] = 1
             
             cons_depth = sum(consensuses.values())
-            mean_fam = sum( [sum(consensus_seq[base_pos][fam].values()) 
-                            for fam in consensus_seq[base_pos]] ) / len(consensus_seq[base_pos])
             ref_freq = (consensuses[(ref_base, ref_base)] / cons_depth) * 100 if (ref_base, ref_base) in consensuses else 0
             
             ref_info = {"contig": contig, "base_pos": base_pos + 1, "ref_base": ref_base}
@@ -321,6 +378,77 @@ def generate_consensus(umi_families, fam_size, ref_seq, contig, region_start, re
                     
 
     return cons_data
+
+
+
+
+
+
+
+#def generate_consensus(umi_families, fam_size, ref_seq, contig, region_start, region_end, bam_file, pos_threshold, percent_threshold, count_threshold, max_depth=1000000, truncate=True, ignore_orphans=True):
+#    """
+#    
+#    
+#    
+#    :param pos_threshold: Window size to group indivual umis into families within groups
+#    
+#    :param percent_threshold: Percent consensus threshold 
+#    
+#    :param count_threshold: Count consensus threshold 
+#    
+#    
+#    Generates consensus data for the given family size and region."""
+#
+#    ## Keys: each base position in the region
+#    ## Values: tables of A,T,C,G (etc) counts from each UMI+Pos family
+#    consensus_seq = get_consensus_seq(umi_families, fam_size, ref_seq, contig, region_start, region_end, bam_file, pos_threshold, max_depth=max_depth, truncate=truncate, ignore_orphans=ignore_orphans)
+#
+#    
+#
+#
+#
+#    cons_data = {}
+#
+#    for base_pos in range(region_start, region_end):
+#
+#        ref_base = ref_seq[base_pos-region_start]
+#
+#        if base_pos in consensus_seq:
+#
+#            consensuses = {}
+#            raw_depth = 0
+#            min_fam = min([sum(consensus_seq[base_pos][fam].values()) for fam in consensus_seq[base_pos]]) 
+#            
+#            for family in consensus_seq[base_pos]:
+#                       
+#                cons_allele = max(consensus_seq[base_pos][family].items(), key = operator.itemgetter(1))[0]
+#                cons_denom = sum(consensus_seq[base_pos][family].values())
+#                cons_percent = (consensus_seq[base_pos][family][cons_allele]/cons_denom) * 100
+#                
+#                raw_depth += cons_denom
+#    
+#                if cons_percent >= percent_threshold and consensus_seq[base_pos][family][cons_allele] >= count_threshold:
+#                    
+#                    if cons_allele in consensuses:
+#                        consensuses[cons_allele] += 1
+#
+#                    else:
+#                        consensuses[cons_allele] = 1
+#            
+#            cons_depth = sum(consensuses.values())
+#            mean_fam = sum( [sum(consensus_seq[base_pos][fam].values()) 
+#                            for fam in consensus_seq[base_pos]] ) / len(consensus_seq[base_pos])
+#            ref_freq = (consensuses[(ref_base, ref_base)] / cons_depth) * 100 if (ref_base, ref_base) in consensuses else 0
+#            
+#            ref_info = {"contig": contig, "base_pos": base_pos + 1, "ref_base": ref_base}
+#            cons_info = consensuses
+#            stats = {"rawdp": raw_depth, "consdp": cons_depth, "min_fam": min_fam, "mean_fam": mean_fam, "ref_freq": ref_freq}
+#                    
+#            row = ConsDataRow(ref_info, cons_info, stats)
+#            cons_data[base_pos] = row
+#                    
+#
+#    return cons_data
 
 
 def generate_uncollapsed(ref_seq, contig, region_start, region_end, bam_file, max_depth=1000000, truncate=True, ignore_orphans=True):
