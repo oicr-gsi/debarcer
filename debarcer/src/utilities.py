b10db34aafca5556da4cf616de650bd7c9b7eac2
rjovelin <richard.jovelin@oicr.on.ca>
2019-07-03 13:16:25 -0400

    added function to retrieve thresholds from config or command

--- debarcer/src/utilities.py
+++ debarcer/src/utilities.py
@@ -38,7 +38,7 @@ def GetInputFiles(configfile, file, filetype):
     :param configfile: Path to config file
     :param file: Path to input file from the command
     "param filetype: Type of input file expected in the config.
-                     Accepted values: prep_file or bam_file
+                     Accepted values: 'prep_file' or 'bam_file'
     '''
     
     # get file from config in priority
@@ -81,6 +81,51 @@ def GetOutputDir(configfile, outputdir):
             # set output directy as current directory
             outdir = os.getcwd()
     return outdir
+
+
+
+def GetThresholds(configfile, parameter, threshold):
+    '''
+    (str, str) -> float
+    
+    :param configfile: Path to config file
+    :param parameter: Parameter name in config file. Accepted values:
+                      'umi_family_pos_threshold'
+                      'umi_edit_distance_threshold'
+                      'percent_consensus_threshold'
+                      'count_consensus_threshold'
+    :param threshold: Setting threshold passed from command    
+    
+    Return a setting threshold
+    '''
+    
+    # get parameter from the config file in priority
+    if parameter in ['umi_family_pos_threshold', 'umi_edit_distance_threshold',
+                     'percent_consensus_threshold', 'count_consensus_threshold']:
+        Level = 'SETTINGS'
+    elif parameter in ['percent_ref_threshold', 'percent_allele_threshold']:
+        Level = 'REPORT'
+        
+    try:
+        config = configparser.ConfigParser()
+        config.read(configfile)
+        ThresholdVal = float(config[Level][parameter])
+    except:
+        # check if threshold privided in command
+        try:
+            ThresholdVal = float(threshold)
+        except:
+            # raise error and exit
+            raise ValueError('ERR: Missing setting threshold')
+    finally:
+        # check that threshold is float
+        if type(ThresholdVal) != float:
+            raise ValueError('ERR: Setting threshold should be float')
+    return ThresholdVal
+
+ ## Lists of umi families with count >= f_size
+#    f_sizes = [int(n) for n in config['SETTINGS']['min_family_sizes'].split(',')] if config else [1, 2, 5]
+
 
 
 def edit_distance(a, b):
