5884798529e711ea337a32e114b10b3bacff4211
rjovelin <richard.jovelin@oicr.on.ca>
2019-08-28 12:35:45 -0400

    trying different color maps

--- debarcer/src/generate_plots.py
+++ debarcer/src/generate_plots.py
@@ -1469,8 +1469,8 @@ def CreateNetworkAx(Columns, Rows, Position, figure, UmiFile):
     nodelist = sorted(degree.keys())
     node_color = [degree[i] for i in nodelist]
     nx.draw_networkx_nodes(G, pos=Pos, with_labels=False, node_size=5, node_color=node_color,
-                           node_shape='o', alpha=0.8, linewidths=0, edgecolors='grey',
-                          ax=None, nodelist=nodelist, cmap=plt.cm.Purples)
+                           node_shape='o', alpha=0.2, linewidths=0, edgecolors='grey',
+                          ax=None, nodelist=nodelist, cmap=plt.cm.Purples_r)
     
       
     
