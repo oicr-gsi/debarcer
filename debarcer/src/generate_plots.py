8bf5dae263aff46c8c532f42dd83a468aaf1699e
rjovelin <richard.jovelin@oicr.on.ca>
2019-09-12 10:10:25 -0400

    add color bar

--- debarcer/src/generate_plots.py
+++ debarcer/src/generate_plots.py
@@ -1043,8 +1043,8 @@ def PlotParentsToChildrenCounts(DataFiles, Outputfile):
     #cb = figure.colorbar(PTU+CTU, cax=cax, orientation = 'horizontal', ticks=[i for i in range(min(node_color), max(node_color)+1)], use_gridspec=False)
     #cb = figure.colorbar(ax, orientation = 'vertical', use_gridspec=False)
     #cb = figure.colorbar()
-    im = ax.imshow(Sizes)
-    cb = plt.colorbar(im)
+    #im = ax.imshow(Sizes)
+    cb = plt.colorbar(Sizes)
     
     cb.set_label('Interval size', size=14, ha='center', color='black', labelpad=18)
     
