a2eee0fb99d288f8253ff943a947e990b64c3c25
rjovelin <richard.jovelin@oicr.on.ca>
2019-09-13 15:05:57 -0400

    debugging

--- debarcer/src/generate_plots.py
+++ debarcer/src/generate_plots.py
@@ -1722,9 +1722,7 @@ def PlotUMiFrequency(L, Outputfile, YLabel, XLabel, Title):
     Plot an histogram of UMI occurence, the number of UMIs occuring 1, 2, .. N times   
     '''
     
-    
-    
-    c = sorted(list(set([L.count(i) for i in L])))
+    c = sorted(list(set([L])))
     print(c)
     
     
