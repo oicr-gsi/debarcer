12a129d149abd3d28e9f0c14a210fac649d1b7ac
rjovelin <richard.jovelin@oicr.on.ca>
2019-09-13 15:01:44 -0400

    debugging

--- debarcer/debarcer.py
+++ debarcer/debarcer.py
@@ -527,6 +527,9 @@ def generate_plots(args):
         # count family size for each family and position  
         umi_occurence = GetUmiFamilyCountFromGrouping(filename)
         Outputfile = os.path.join(FigDir, 'UMI_family_freq_distribution_{0}.{1}'.format(region, args.extension))
+        
+        print(region)
+        
         PlotUMiFrequency(umi_occurence, Outputfile, 'Counts', 'Read count', region)
                 
         # plot marginal distributions of UMI family size and read depth
