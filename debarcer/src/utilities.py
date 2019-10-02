8cb0ff2a80a1157e5d5fb4b3353511487bf61951
rjovelin <richard.jovelin@oicr.on.ca>
2019-10-02 15:17:29 -0400

    fixed bug

--- debarcer/src/utilities.py
+++ debarcer/src/utilities.py
@@ -420,13 +420,12 @@ def CheckJob(JobName):
     # make a sorted list of accounting files with job info archives
     try:
         result = subprocess.check_output('qstat -j {0} | grep job_name'.format(JobName), shell=True).decode('utf-8').rstrip()
-        
-        print(result)
-        
         if 'job_name' in result:
             result = result.split()
             assert result[1].strip() == JobName
             running = True
+        else:
+            running = False
     except:
         running = False 
     return running
