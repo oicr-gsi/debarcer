0f41de99466f38e3dd0c4639424a4f79ce78a66a
rjovelin <richard.jovelin@oicr.on.ca>
2019-10-11 19:19:14 -0400

    added title

--- debarcer/debarcer.py
+++ debarcer/debarcer.py
@@ -569,7 +569,8 @@ def generate_plots(args):
         # plot non-reference frequency limiting Y axis to 20% for visualization of low-frequency variants 
         Outputfile = os.path.join(FigDir, 'NonRefFreq_low_freq_{0}.{1}'.format(region, args.extension))
         plt.clf(), plt.cla()
-        PlotNonRefFreqData(filename, Colors, Outputfile, YLimit=10)
+        freqlimit = 10
+        PlotNonRefFreqData(filename, Colors, Outputfile, YLimit=freqlimit, title='cropping frequencies at {0}%'.format(freqlimit))
         
         
         print('PlotConsDepth')
