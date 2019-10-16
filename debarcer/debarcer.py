19795c6d19c7909bd4e95714cdc57f7501c78c6c
rjovelin <richard.jovelin@oicr.on.ca>
2019-10-16 12:49:02 -0400

    debugging

--- debarcer/debarcer.py
+++ debarcer/debarcer.py
@@ -560,7 +560,7 @@ def generate_plots(args):
         # plot non-reference frequency
         Outputfile = os.path.join(FigDir, 'NonRefFreq_{0}.{1}'.format(region, args.extension))
         plt.clf(), plt.cla()
-        PlotNonRefFreqData(filename, Colors, Outputfile)
+        PlotNonRefFreqData(filename, Colors, Outputfile, ylabel='right center')
     
     
         print('PlotNonRefFreqData LOW')
