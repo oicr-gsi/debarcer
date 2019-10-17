95459236033c9a4c1ae2dbc0eba69e9253f24664
rjovelin <richard.jovelin@oicr.on.ca>
2019-10-17 12:03:42 -0400

    testing new coverage plot

--- debarcer/src/generate_report.py
+++ debarcer/src/generate_report.py
@@ -13,6 +13,8 @@ from itertools import zip_longest
 import base64
 import matplotlib
 import pygal
+import PIL
+
 
 def ResizeFifure(filename, scaling_factor):
     '''
@@ -877,20 +879,34 @@ def WriteReport(directory, extension, Outputfile, **Options):
 
     dot_chart = pygal.Dot(x_label_rotation=30)
     dot_chart.title = 'V8 benchmark results'
-    dot_chart.x_labels = ['Richards', 'DeltaBlue', 'Crypto', 'RayTrace', 'EarleyBoyer', 'RegExp', 'Splay', 'NavierStokes']
+    #dot_chart.x_labels = ['Richards', 'DeltaBlue', 'Crypto', 'RayTrace', 'EarleyBoyer', 'RegExp', 'Splay', 'NavierStokes']
     dot_chart.add('Chrome', [6395, 8212, 7520, 7218, 12464, 1660, 2123, 8607])
     dot_chart.add('Firefox', [7473, 8099, 11700, 2651, 6361, 1044, 3797, 9450])
     dot_chart.add('Opera', [3472, 2933, 4203, 5229, 5810, 1828, 9013, 4669])
     dot_chart.add('IE', [43, 41, 59, 79, 144, 136, 34, 102])
     #dot_chart.render()
-    source_image = dot_chart.render_data_uri()
+    #source_image = dot_chart.render_data_uri()
     # Return `data:image/svg+xml;charset=utf-8;base64,...`
 
+    dot_chart.render_to_file('/u/rjovelin/test_chart.svg')    
+    
+    
+    
+    height, width = ResizeFifure('/u/rjovelin/test_chart.svg', 0.7)
+    source_image = EncodeImage('/u/rjovelin/test_chart.svg')
+      
+    
     
 
     L2 = [markdown(i) for i in L]
     #L2.append('<img style="padding-right: 10px; padding-left:10px" src={0}" alt="{1}"/>'.format(source_image, 'test'))
-    L2.append('<embed type="image/svg+xml" src= {0} />'.format(source_image))
+    
+    L2.append('<embed type="image/svg+xml" src= {0} width="{1}" height="{2}" />'.format(source_image, width, height))
+    
+    
+       
+    
+    #L2.append('<embed type="image/svg+xml" src= {0} />'.format(source_image))
     
     # create report string
     #S = ''.join([markdown(i) for i in L])
