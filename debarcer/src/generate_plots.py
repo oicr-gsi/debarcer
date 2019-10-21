b317be3ac8683da2242c7e21ac4218aae4dafef3
rjovelin <richard.jovelin@oicr.on.ca>
2019-10-21 18:38:19 -0400

    testing new font

--- debarcer/src/generate_plots.py
+++ debarcer/src/generate_plots.py
@@ -435,6 +435,7 @@ def PlotDataPerRegion(CoverageStats, DataFiles, **Options):
                          no_data_font_size=40, no_data_text='No result found',
                          label_font_size=12,
                          background = 'white',
+                         font_family='Arial, Verdana, sans-serif',
                          base_style=DefaultStyle)
     # set number of columns for legend
     if len(low_data) != 0:
