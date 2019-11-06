0368d74db9744edd1c2904cb668e130a65a6b387
rjovelin <richard.jovelin@oicr.on.ca>
2019-11-06 15:56:13 -0500

    do not force plotting with startangle

--- debarcer/src/generate_plots.py
+++ debarcer/src/generate_plots.py
@@ -579,21 +579,23 @@ def CreateNonRefFreqAx(Columns, Rows, Position, figure, Data, Color, fam_size, *
         except:
             YMax = [Data[i] for i in Data]
             YMax = max(YMax)
+    
+        finally:
+             print('ymax', YMax)
+    
+    
+    
     else:
         YMax = [Data[i] for i in Data]
         YMax = max(YMax)
     
-    print('ymax', YMax)
+   
     
     
     
     YMax = float(YMax + (YMax * 10 /100))
     ax.set_ylim([0, YMax])    
      
-    print('ymax adjusted', YMax)
-    
-    
-    
     # write y axis ticks
     if YMax <=2:
         ax.yaxis.set_ticks([i for i in np.arange(0, YMax, 0.2)])
@@ -1875,8 +1877,7 @@ def PlotIncorrectReads(ReadInfo, Outputfile, datatype, W, H):
     # plot data as pie chart
     ax.pie(size, labels=labels, colors=colors,
            textprops={'fontsize':22, 'fontweight':'normal'},
-           wedgeprops = { 'linewidth' : 7, 'edgecolor' : 'white' },
-           startangle=90)
+           wedgeprops = { 'linewidth' : 7, 'edgecolor' : 'white' })
     # add circle in the center of the pie to create a donut
     p=plt.gcf()
     p.gca().add_artist(my_circle)
