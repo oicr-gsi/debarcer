b34bceee43be8aff6f59c3982978b52155aa09e7
rjovelin <richard.jovelin@oicr.on.ca>
2019-11-11 12:45:32 -0500

    raise error if unexpected or missing values in library prep. #146

--- debarcer/src/preprocess_fastqs.py
+++ debarcer/src/preprocess_fastqs.py
@@ -33,9 +33,15 @@ def check_library_prep(prepname, prepfile):
     D = {i:j for i, j in L.items()} 
     # check that all expected parameters are present
     expected = {'input_reads', 'output_reads', 'umi_locs', 'umi_lens', 'spacer', 'spacer_seq', 'umi_pos'}
-    missing = set(D.keys()).symmetric_difference(set(expected))
+    
+    unexpected = set(D.keys()).difference(set(expected))
+    if len(unexpected) != 0:
+        raise ValueError('ERR: unexpected keys in librabry prep: {0}'.format(', '.join(unexpected)))
+    missing = set(expected).symmetric_difference(set(D.keys()))
     if len(missing) != 0:
-        raise ValueError('ERR: unexpected keys in librabry prep'.format(', '.join(missing)))
+        raise ValueError('ERR: missing keys in librabry prep: {0}'.format(', '.join(missing)))
+       
+    
     # check that umi_locs, umi_lens and umi_pos have same length
     a = list(map(lambda x: x.strip(), D['umi_locs'].split(',')))
     b = list(map(lambda x: x.strip(), D['umi_lens'].split(',')))
@@ -308,8 +314,8 @@ def reheader_fastqs(r1_file, outdir, prepname, prepfile, **KeyWords):
         readseqs = [i[1] for i in reads]
         umis = extract_umis(readseqs, umi_locs, umi_lens, umi_pos)
         
-        print(reads)
-        print(umis)
+#        print(reads)
+#        print(umis)
         
         
         # skip reads with spacer in wrong position
@@ -339,15 +345,15 @@ def reheader_fastqs(r1_file, outdir, prepname, prepfile, **KeyWords):
         UmiLength, SpacerLength = [umi_len_r1, umi_len_r2], [spacer_len_r1, spacer_len_r2]    
         
         
-        print(UmiLength)
-        print(SpacerLength)
-        print(readnames)
-        print(namerests)
+#        print(UmiLength)
+#        print(SpacerLength)
+#        print(readnames)
+#        print(namerests)
         
         
         for i in range(len(writers)):
             
-            print(readnames[i] + ":" + ';'.join(umis) + " " + namerests[i] + "\n")
+#            print(readnames[i] + ":" + ';'.join(umis) + " " + namerests[i] + "\n")
             
             # add umi to read name and write to outputfile
             writers[i].write(readnames[i] + ":" + ';'.join(umis) + " " + namerests[i] + "\n")
@@ -358,19 +364,19 @@ def reheader_fastqs(r1_file, outdir, prepname, prepfile, **KeyWords):
             elif i > 0:
                 k = -1
                 
-            print(i, k)
-            print(UmiLength[i], SpacerLength[i])
-            print(reads[k][1])
-            print(reads[k][1][UmiLength[i] + SpacerLength[i]:])
-            print(reads[k][2])    
-            print(reads[k][3])
+#            print(i, k)
+#            print(UmiLength[i], SpacerLength[i])
+#            print(reads[k][1])
+#            print(reads[k][1][UmiLength[i] + SpacerLength[i]:])
+#            print(reads[k][2])    
+#            print(reads[k][3])
             
             writers[i].write(reads[k][1][UmiLength[i] + SpacerLength[i]:])
             writers[i].write(reads[k][2])
             writers[i].write(reads[k][3][UmiLength[i] + SpacerLength[i]:])
         
         
-        assert 0 > 1
+#        assert 0 > 1
         
         
         
