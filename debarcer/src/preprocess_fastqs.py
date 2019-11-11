519f3798f3c1ab48aee8c0a8fa40d8d628888209
rjovelin <richard.jovelin@oicr.on.ca>
2019-11-11 12:21:40 -0500

    addressing issue #146

--- debarcer/src/preprocess_fastqs.py
+++ debarcer/src/preprocess_fastqs.py
@@ -307,6 +307,11 @@ def reheader_fastqs(r1_file, outdir, prepname, prepfile, **KeyWords):
         # make a list of read sequences
         readseqs = [i[1] for i in reads]
         umis = extract_umis(readseqs, umi_locs, umi_lens, umi_pos)
+        
+        print(reads)
+        print(umis)
+        
+        
         # skip reads with spacer in wrong position
         if spacer == True and correct_spacer(readseqs, umis, spacer_seq) == False:
             # count reads with incorrect umi/spacer configuration
@@ -333,7 +338,17 @@ def reheader_fastqs(r1_file, outdir, prepname, prepfile, **KeyWords):
         # make lists with umi lengths and spacer lengths    
         UmiLength, SpacerLength = [umi_len_r1, umi_len_r2], [spacer_len_r1, spacer_len_r2]    
         
+        
+        print(UmiLength)
+        print(SpacerLength)
+        print(readnames)
+        print(namerests)
+        
+        
         for i in range(len(writers)):
+            
+            print(readnames[i] + ":" + ';'.join(umis) + " " + namerests[i] + "\n")
+            
             # add umi to read name and write to outputfile
             writers[i].write(readnames[i] + ":" + ';'.join(umis) + " " + namerests[i] + "\n")
             # remove umi and spacer from read seq. write reamining of read to outputfile
@@ -342,10 +357,23 @@ def reheader_fastqs(r1_file, outdir, prepname, prepfile, **KeyWords):
                 k = 0
             elif i > 0:
                 k = -1
+                
+            print(i, k)
+            print(UmiLength[i], SpacerLength[i])
+            print(reads[k][1])
+            print(reads[k][1][UmiLength[i] + SpacerLength[i]:])
+            print(reads[k][2])    
+            print(reads[k][3])
+            
             writers[i].write(reads[k][1][UmiLength[i] + SpacerLength[i]:])
             writers[i].write(reads[k][2])
             writers[i].write(reads[k][3][UmiLength[i] + SpacerLength[i]:])
-            
+        
+        
+        assert 0 > 1
+        
+        
+        
     # close all open files
     for i in writers:
         i.close()
