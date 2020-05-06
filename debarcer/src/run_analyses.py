9c8540d420e2530e8919acd48a0583a7b9bcc360
rjovelin <richard.jovelin@oicr.on.ca>
2020-05-06 13:27:50 -0400

    fixing issue launching jobs

--- debarcer/src/run_analyses.py
+++ debarcer/src/run_analyses.py
@@ -272,7 +272,12 @@ def submit_jobs(bamfile, outdir, reference, famsize, bedfile, count_threshold,
     GroupCmd = '{0} {1} group -o {2} -r \"{3}\" -b {4} -d {5} -p {6} -i {7} -t {8} -s {9}'
     # set up collapse cmd
     CollapseCmd = 'sleep 60; {0} {1} collapse -o {2} -b {3} -r \"{4}\" -u {5} -f \"{6}\" -ct {7} -pt {8} -p {9} -m {10} -t {11} -i {12} -stp {13} -s {14}'
-        
+    
+
+    print('project', project)
+    print('queue', queue)
+
+    
     # set qsub command
     if project == '':
         # run jobs on sge
