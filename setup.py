fc7d8c7eb01d933a444fc0d2515a266f723d1ad5
rjovelin <richard.jovelin@oicr.on.ca>
2020-09-14 08:36:45 -0400

    added entry point

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
 	"Operating System :: Unix",
 	"Operating System :: MacOS",
 	],
+    entry_points={'console_scripts': ['debarcer = debarcer']},
 	install_requires = ["numpy>=1.14.2", "pandas>=0.22.0", "pysam>=0.14.1",
                         "matplotlib>=3.1.2", "mistune>=0.8.4", "networkx>=1.11",
                         "pygal>=2.4.0", "scipy>=1.0.1", "seaborn>=0.9.0",
