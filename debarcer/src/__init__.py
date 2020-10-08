a98916d59dabe4e9be6851d5dd923abb140c67a4
rjovelin <richard.jovelin@oicr.on.ca>
2020-10-08 12:29:08 -0400

    re-packaging

--- debarcer/src/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep 14 15:49:30 2020
-
-@author: rjovelin
-"""
-
--- debarcer/src/version.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 28 14:57:35 2019
-
-@author: RJovelin
-"""
-
-# setup and code access debarcer version from this module
-__version__ = "2.4.5"
